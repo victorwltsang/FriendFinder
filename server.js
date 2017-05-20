8ebdc122550b28ff87558053fbc98b2cad117961
dblanchard13 <davidb.prof@gmail.com>
2017-05-19 23:49:22 -0700

    Homework comments.

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // declare dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+// you aren't actually using the path module in this file so there's no need to require it
+// var path = require('path');
 
 //initiate express app
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended:false}));
+// by setting the extended option to `true`, you can treat rich objects and arrays just like any other property on the request body
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 
@@ -16,5 +18,5 @@ require("./app/routing/htmlRoutes.js")(app);
 
 
 app.listen(PORT,function(){
-  console.log("Listening on PORT%s",PORT);
+  console.log("Listening on PORT %s",PORT);
 });
